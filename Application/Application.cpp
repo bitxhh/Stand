184965255c41b968a9dafaf9074a42c85ff47b3d
Banshiev <112979617+bitxhh@users.noreply.github.com>
2025-12-09 02:07:29 +0300

    Pass Device references to UI windows

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -3,16 +3,16 @@
 #include <algorithm>
 #include <QSize>
 
-DeviceDetailWindow::DeviceDetailWindow(std::string serial, std::string info, LimeManager& manager, QWidget* parent)
+DeviceDetailWindow::DeviceDetailWindow(std::shared_ptr<Device> device, LimeManager& manager, QWidget* parent)
     : QMainWindow(parent)
-    , serial(std::move(serial))
+    , device(std::move(device))
     , manager(manager) {
-    setWindowTitle("LimeSDR Device");
+    setWindowTitle(QString::fromStdString(this->device->GetSerial()));
     auto* central = new QWidget(this);
     auto* layout = new QVBoxLayout(central);
 
-    auto* infoLabel = new QLabel(QString("Serial: %1").arg(QString::fromStdString(this->serial)), central);
-    auto* detailLabel = new QLabel(QString("Info: %1").arg(QString::fromStdString(info)), central);
+    auto* infoLabel = new QLabel(QString("Serial: %1").arg(QString::fromStdString(this->device->GetSerial())), central);
+    auto* detailLabel = new QLabel(QString("Info: %1").arg(QString::fromStdString(std::string(this->device->GetInfo()))), central);
 
     layout->addWidget(infoLabel);
     layout->addWidget(detailLabel);
@@ -28,8 +28,8 @@ DeviceDetailWindow::DeviceDetailWindow(std::string serial, std::string info, Lim
 void DeviceDetailWindow::checkDeviceConnection() {
     manager.refresh_devices();
     const auto& devices = manager.get_devices();
-    const bool connected = std::any_of(devices.begin(), devices.end(), [&](const Device& device) {
-        return device.GetSerial() == serial;
+    const bool connected = std::any_of(devices.begin(), devices.end(), [&](const std::shared_ptr<Device>& managedDevice) {
+        return managedDevice->GetSerial() == device->GetSerial();
     });
 
     if (!connected) {
@@ -41,7 +41,7 @@ void DeviceDetailWindow::checkDeviceConnection() {
 DeviceSelectionWindow::DeviceSelectionWindow(LimeManager& manager, QWidget* parent)
     : QWidget(parent)
     , manager(manager) {
-    setWindowTitle("Select LimeSDR Device");
+    setWindowTitle("LimeManager VSU");
 
     auto* layout = new QVBoxLayout(this);
     statusLabel = new QLabel("Searching for LimeSDR devices...", this);
@@ -74,21 +74,20 @@ void DeviceSelectionWindow::refreshDevices() {
         auto* item = new QListWidgetItem(deviceList);
         item->setSizeHint(QSize(0, 40));
 
-        const std::string serial = device.GetSerial();
-        const std::string info = device.GetInfo();
+        const std::string serial = device->GetSerial();
 
         auto* button = new QPushButton(QString::fromStdString(serial), deviceList);
-        connect(button, &QPushButton::clicked, this, [this, serial, info]() {
-            openDevice(serial, info);
+        connect(button, &QPushButton::clicked, this, [this, device]() {
+            openDevice(device);
         });
 
         deviceList->setItemWidget(item, button);
     }
 }
 
-void DeviceSelectionWindow::openDevice(const std::string& serial, const std::string& info) {
+void DeviceSelectionWindow::openDevice(const std::shared_ptr<Device>& device) {
     refreshTimer->stop();
-    auto* window = new DeviceDetailWindow(serial, info, manager);
+    auto* window = new DeviceDetailWindow(device, manager);
     window->setAttribute(Qt::WA_DeleteOnClose);
     connect(window, &DeviceDetailWindow::deviceDisconnected, this, [this, window]() {
         QMessageBox::warning(window, "Device disconnected", "Connection to the device was lost. Returning to device search.");
